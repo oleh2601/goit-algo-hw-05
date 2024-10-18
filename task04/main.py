8cffe32625e5b3e7cb650337a17c700406da47cf
dos_santos <nobackups@protonmail.com>
2024-10-18 21:42:00 +0300

    solved task04

--- task04/main.py
+++ task04/main.py
@@ -1,51 +1,85 @@
 import sys
 from functools import wraps
 
-
 def input_error(func):
     def inner(*args, **kwargs):
         try:
             return func(*args, **kwargs)
         except ValueError:
-            return "Give me name and phone please."
+            return "Please provide name and phone."
+        except IndexError:
+            return "Please provide name and phone."
 
     return inner
 
+@input_error
+def add_contact(args, contacts: dict) -> str:
+    name, phone = args
+    contacts[name] = phone
+    return "Contact added."
 
+def get_all_contacts_error(func):
+    def wrapper(contacts):
+        try:
+            if not contacts:
+                return "No contacts found."
+            return func(contacts)
+        except TypeError:
+            return "No contacts found."
+    return wrapper
+        
+@get_all_contacts_error
+def get_all_contacts(contacts: dict) -> str:
+    if contacts:
+        return "\n".join([f"{name}: {phone}" for name, phone in contacts.items()])
+    
+def get_phone_error(func):
+    def wrapper(*args):
+        try:
+            if not args or not args[0]:
+                return "Please provide the name of the contact."
+            name_args = args[0]
+            if not name_args:
+                return "Please provide the name of the contact."
+            name = name_args[0]
+            contacts = args[1]
+            return func(name, contacts)  
+        except KeyError:
+            return "This contact does not exist."
+        except IndexError:
+            return "Please provide the name of the contact."
+        except TypeError:
+            return "Please provide the name of the contact."
+    return wrapper
 
-#getting the command and the arguments from the user
+@get_phone_error
+def get_phone(name, contacts: dict) -> str:
+    return contacts[name]
+
 def parse_input(user_input):
     cmd, *args = user_input.split()
     cmd = cmd.strip().lower()
     return cmd, *args
-#converting our dictionary to a string
-def get_all_contacts(contacts: dict) -> str:
-    if contacts:
-        return "\n".join([f"{name}: {phone}" for name, phone in contacts.items()])
-    else:
-        return "No contacts found."
-#checking if the key exists in our dic, returning the value
-def get_phone(name, contacts: dict) -> str:
-    if name in contacts:
-        return contacts[name]
-    else:
-        return ("There is no contact with such name")
-    
-#changing the value of the existing item in the dictionary
+
+def change_contact_error(func):
+    def inner(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except ValueError:
+            return "Please provide name and phone."
+        except IndexError:
+            return "Please provide name and phone."
+        except TypeError:
+            return "Please provide name and phone."
+
+    return inner
+
+@change_contact_error
 def change_contact(args, contacts: dict) -> str:
     name, phone = args
     contacts[name] = phone
     return "Contact changed."
 
-#adding new contact to existic dictionary
-#checking for correct arguments amount
-def add_contact(args, contacts: dict) -> str:
-    if len(args) == 2:
-        name, phone = args
-        contacts[name] = phone
-        return "Contact added."
-    else:
-        return "Invalid input. Usage: add username phone"
 
 def main():
     contacts = {}
@@ -55,41 +89,35 @@ def main():
 
         user_input = input("Enter a command: ")
         command, *args = parse_input(user_input)
-        #our only condition to exit
-        if command in ["close", "exit"]:
-            print("Good bye!")
-            sys.exit(0)
-        elif command == "hello":
-            print("How can I help you?")
-        #for each command below checking if the user 
-        #is passing the right amount of arguments
-        elif command == "add":
-            if len(args) == 2:
-                print(add_contact(args, contacts))
-            else:
-                print('Wrong syntax: add *username* *phone*')        
-        elif command == "change":
-            if len(args) == 2:
-                if args[0] in contacts:
-                    print(change_contact(args, contacts))
-                else:
-                    print("There is no contact with such name.")
-            else:
-                print("Wrong syntax: change *username* *phone*")     
-        elif command == "phone":
-            if len(args) == 1:
-                print(get_phone(args[0], contacts))
-            else:
-                print('Wrong syntax: phone *username*')
-        elif command == "all":
-            if len(args) > 0:
-                print('Wrong syntax: all')
-            elif not contacts:
-                print('No contacts found')
-            else:
+        match command:
+            case "close" | "exit":
+                print("Good bye!")
+                sys.exit(0)
+            case "hello":
+                print("How can I help you?")
+            case "add":
+                print(add_contact(args, contacts))     
+            case "change":
+                print(change_contact(args, contacts))  
+            case "phone":
+                print(get_phone(args, contacts))
+            case "all":
                 print(get_all_contacts(contacts))
-        else:
-            print("Invalid command.")
+            case "help":
+                print('Available commands: \n'
+                      '  "add" username number \n'
+                      '  "all" \n'
+                      '  "change" username number \n'
+                      '  "close" \n'
+                      '  "exit" \n'
+                      '  "help" \n'
+                      '  "hello" \n'
+                      '  "phone" username \n'
+                       )
+            case _:
+                print("I don't know this command.\n"
+                      "Use 'help' to get the list of all commands."
+                      )
 
 if __name__ == "__main__":
     main()
